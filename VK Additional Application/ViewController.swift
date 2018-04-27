1f122262335ecf318a11cf8ff696b9960792da8f
Artem Kufaev <KufaevArtem@icloud.com>
2018-04-27 23:24:34 +0400

    Initial Commit

---
+++ VK Additional Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VK Additional Application
+//
+//  Created by Артем on 27.04.2018.
+//  Copyright © 2018 NONE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
