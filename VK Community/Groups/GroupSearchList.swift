f372dc066fd8bd6ac44aadf806a339b298d93740
Nelmeris <KufaevArtem@icloud.com>
2018-05-03 21:24:50 +0400

    Организация поиска при выборе новой группы

--- VK Community/Groups/GroupSearchList.swift
+++ VK Community/Groups/GroupSearchList.swift
@@ -8,16 +8,50 @@
 
 import UIKit
 
-class SearchGroupList: UITableViewController {
+class SearchGroupList: UITableViewController, UISearchBarDelegate {
+    var currentGroups = groups
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchBar.delegate = self
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if !searchText.isEmpty {
+            currentGroups = groups.filter({ group -> Bool in
+                return group.lowercased().contains(searchText.lowercased())
+            })
+        } else {
+            currentGroups = groups
+        }
+        
+        tableView.reloadData()
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return groups.count
+        return currentGroups.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Group", for: indexPath) as! GroupCell
         
-        cell.groupName.text = groups[indexPath.row]
+        cell.groupName.text = currentGroups[indexPath.row]
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
     }
 }
